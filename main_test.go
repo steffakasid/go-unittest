5f142295f001944b9aa78be96e2d93a03245da98
Steffen Rumpf <steffen.rumpf@deutschebahn.com>
2024-02-13 11:16:27 +0100

    feat: test something with table driven tests

--- main_test.go
+++ main_test.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func setupAndTeardownTest(t *testing.T) func() {
@@ -18,5 +16,74 @@ func TestMultiply(t *testing.T) {
 
 	t.Log("Testing now Multiply")
 	res := multiply(3, 3)
-	assert.Equal(t, 9, res)
+	if res != 9 {
+		t.Fatal("Res must be 9")
+	}
+}
+
+func TestTableDoesntWork(t *testing.T) {
+	name := "this does not work works"
+	type tableTest struct {
+		name   *string
+		val1   int
+		val2   int
+		result int
+	}
+	testDefinition := []tableTest{
+		{
+			val1:   1,
+			val2:   2,
+			result: 2,
+		},
+		{
+			name:   &name,
+			val1:   1,
+			val2:   2,
+			result: 2,
+		},
+	}
+
+	for _, test := range testDefinition {
+		// Nill pointer at this point for test1
+		t.Run(*test.name, func(t *testing.T) {
+			res := multiply(test.val1, test.val2)
+			if res != test.result {
+				t.Fatal("Res must be", test.result)
+			}
+		})
+	}
+}
+
+func TestTable(t *testing.T) {
+	name := "this works"
+	type tableTest struct {
+		name   *string
+		val1   int
+		val2   int
+		result int
+	}
+	testDefinition := []tableTest{
+		{
+			name:   &name,
+			val1:   1,
+			val2:   2,
+			result: 2,
+		},
+		{
+			name:   &name,
+			val1:   1,
+			val2:   2,
+			result: 2,
+		},
+	}
+
+	for _, test := range testDefinition {
+		t.Run(*test.name, func(t *testing.T) {
+			t.Log("Testing now Multiply", test.name)
+			res := multiply(test.val1, test.val2)
+			if res != test.result {
+				t.Fatal("Res must be", test.result)
+			}
+		})
+	}
 }
